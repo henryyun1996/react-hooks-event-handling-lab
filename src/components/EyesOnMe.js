00886438dac9154412f618a87256120a456d8469
Henry <henrysungyun@gmail.com>
2023-02-21 21:42:19 -0800

    getting it

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function handleFocus(event) {
+        console.log('Good!');
+    }
+    function handleBlur(event) {
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <div onFocus={handleFocus} onBlur={handleBlur}>
+            <button>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
